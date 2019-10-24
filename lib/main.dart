05bbe5b052ff75e4751dd217a08c9d483904bda6
abhirup-deb <debabhirup1999@gmail.com>
2019-10-24 09:55:46 +0530

    First Web App using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+
+class Home extends StatefulWidget{
+  Home({Key key}) : super(key:key);
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(5.0),
+            height: 80.0,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.indigo,
+          ),
+        ],
+      ),
+    );
+  }
+}
