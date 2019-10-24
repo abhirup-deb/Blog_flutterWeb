fe79e6ac32e050e4e21fe533fc15b8f001b28ac5
abhirup-deb <debabhirup1999@gmail.com>
2019-10-24 10:40:59 +0530

    HomePage Linked

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,7 @@
 import 'package:flutter/material.dart';
+import 'Homepage.dart';
 
 
-
-class Home extends StatefulWidget{
-  Home({Key key}) : super(key:key);
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home>{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(5.0),
-            height: 80.0,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.indigo,
-          ),
-        ],
-      ),
-    );
-  }
+void main(){
+  runApp(MaterialApp(home: Home(),));
 }
